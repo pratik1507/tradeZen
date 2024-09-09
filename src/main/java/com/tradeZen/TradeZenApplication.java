3c071870f886b4d1dc0fd78763f483c310042540
Powar Pratik Ravsaheb <2100032537@kluniversity.in>
2024-09-09 23:11:56 +0530

    boilerplate

---
+++ src/main/java/com/tradeZen/TradeZenApplication.java
@@ -0,0 +1,13 @@
+package com.tradeZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradeZen/TradeZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradeZen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
